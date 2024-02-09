9c576711dfb6bdf78db0a9cd0a2b5a393378752c
NguyenBang <comtrang.gioson.giolinh@gmail.com>
2024-02-10 03:17:12 +0700

    add disconnect db after determining the SIGTERM signal

--- backend/configs/db.go
+++ backend/configs/db.go
@@ -3,6 +3,10 @@ package config
 import (
 	"backend/db"
 	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type DB struct {
@@ -15,11 +19,17 @@ func connectDB(client *db.PrismaClient) error {
 		return nil
 	}
 
-	// defer func() {
-	// 	if err := client.Prisma.Disconnect(); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
+	// Disconnect DB before shutting down the application
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("Toi dang disconnect db")
+		if err := client.Prisma.Disconnect(); err != nil {
+			panic(fmt.Errorf("could not disconnect: %w", err))
+		}
+		os.Exit(0)
+	}()
 	return nil
 }
 
